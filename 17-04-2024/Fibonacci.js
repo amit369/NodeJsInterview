d8641beb009467ea253eb3f79c8a9289285211ab
amit-chaman <amitchaman369@gmail.com>
2024-04-18 18:54:36 +0530

    Changes done

---
+++ 17-04-2024/Fibonacci.js
@@ -0,0 +1,14 @@
+
+function generateFiboncciSeries(n)
+{       
+    let n1=0,n2=1,n3;
+    for(let i=0;i<n;i++)
+    {
+       console.log(n1);
+       n3=n1+n2;
+       n1=n2;
+       n2=n3;
+    }
+}
+
+generateFiboncciSeries(10);
---
+++ 17-04-2024/StringReverseIterative.js
@@ -0,0 +1,9 @@
+
+let str="amit chaman", rev="";
+
+for(let n=str.length-1;n>=0;n--)
+{
+    rev += str[n];
+}
+
+console.log(rev);
