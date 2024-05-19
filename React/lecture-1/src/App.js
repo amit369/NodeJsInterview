81759ffa32c27edca9bac4796ef21ae8cf226567
amit-chaman <amitchaman369@gmail.com>
2024-05-19 13:43:49 +0530

    Changes done

--- React/lecture-1/src/App.js
+++ React/lecture-1/src/App.js
@@ -4,13 +4,20 @@ import { useState } from 'react';
 
 function App() {
   const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+  }
   return (
     <div className="App">
             <div>
+              <form onSubmit={handleSubmit}>
               <label> Enter your name :- 
               <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
               </label>
-             
+              <input type="submit"></input>
+              </form>
             </div>
             {name}
     </div>
---
+++ This.js
@@ -0,0 +1,10 @@
+
+function abc()
+{
+    console.log(this);
+}
+console.log(this);
+abc();
+
+var a=10;
+console.log(a);
