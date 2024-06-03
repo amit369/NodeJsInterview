a9fd839cc4b2444f940f6c6725d973d1988cd5ce
amit-chaman <amitchaman369@gmail.com>
2024-06-03 10:16:42 +0530

    changes ok

---
+++ 03-06-2024/Fibonacci.js
@@ -0,0 +1,15 @@
+
+function generateFibonacciSeries(val)
+{
+    let n1=0,n2=1,n3;
+
+    for(let i=0;i<val;i++)
+        {
+            n3 = n1+n2;
+            console.log(n1);
+            n1=n2;
+            n2=n3;
+        }
+}
+
+generateFibonacciSeries(10);
