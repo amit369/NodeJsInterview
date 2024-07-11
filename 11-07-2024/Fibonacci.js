73c66cbed305adf59bc0c1822d0540eb5ad6a2ef
amit-chaman <amitchaman369@gmail.com>
2024-07-11 12:23:16 +0530

    changes done

---
+++ 11-07-2024/Fibonacci.js
@@ -0,0 +1,14 @@
+
+function printFibonacciSeries(n)
+{
+var n1=0,n2=1,n3;
+for(let i=0;i<n;i++)
+{
+    console.log(n1);
+    n3 = n1 + n2;
+    n1=n2;
+    n2=n3;
+}
+}
+
+printFibonacciSeries(20);
