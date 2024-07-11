4ba2c152ff3a57276a060f8d751e827e4df046d8
amit-chaman <amitchaman369@gmail.com>
2024-07-11 12:36:42 +0530

    changes done

---
+++ 11-07-2024/CountCharacters.js
@@ -0,0 +1,15 @@
+let str = "my name is amit chaman";
+let count = {};
+
+for(let i=0;i<str.length;i++)
+{
+    count[str[i]] = 0;
+}
+
+
+for(let i=0;i<str.length;i++)
+    {
+        count[str[i]] += 1;
+    }
+
+console.log(' count ', count);
