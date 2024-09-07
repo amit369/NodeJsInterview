4566f51401cafc9ca6184347080d819858a043eb
amit-chaman <amitchaman369@gmail.com>
2024-09-07 19:52:33 +0530

    Changes done

--- Web-Development/7-09-2024/client/src/App.js
+++ Web-Development/7-09-2024/client/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import User from './User';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-             
-           <User />
+               
       </header>
     </div>
   );
